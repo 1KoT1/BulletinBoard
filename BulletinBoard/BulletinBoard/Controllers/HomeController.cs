b6e47d42f9e049550fa2984fb7f449e91ab38c06
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-09-19 18:50:28 +0400

    Прикрутил журналирование.

---
+++ BulletinBoard/BulletinBoard/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BulletinBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            log.Debug("Run Index()");
+            return View();
+        }
+
+        ILog log = LogManager.GetLogger(typeof(HomeController));
+    }
+}
