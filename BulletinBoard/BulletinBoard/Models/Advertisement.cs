3e247f1bfc8a310e54502344e1bada030d626b6d
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-09-20 05:41:00 +0400

    Примерно скидал классы объявлений.

---
+++ BulletinBoard/BulletinBoard/Models/Advertisement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BulletinBoard.Models
+{
+    public class Advertisement
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public uint Price { get; set; }
+        public DateTime PublishDate { get; set; }
+        public Contacts Contacts { get; set; }
+    }
+}
