e025abc9527307b0f8be4c378474595a66eaea74
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-09-21 21:30:05 +0800

    Вся валидация будет в одном месте в контроллере.

--- BulletinBoard/BulletinBoard/ViewModels/CreateAdvertisementView.cs
+++ BulletinBoard/BulletinBoard/ViewModels/CreateAdvertisementView.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,17 +7,9 @@ namespace BulletinBoard.ViewModels
 {
     public class CreateAdvertisementView
     {
-
-        [Required(ErrorMessage = "Введите заголовок объявления.")]
         public string Name { get; set; }
-
-        [Required(ErrorMessage = "Введите текст объявления.")]
         public string Description { get; set; }
-
-        [Required(ErrorMessage = "Укажите цену товара.")]
         public int Price { get; set; }
-        
-        [Required(ErrorMessage = "Укажите свои контактые данные.")]
         public string Contacts { get; set; }
     }
 }
