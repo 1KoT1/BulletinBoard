7dc040d5f5cfb1561038fcba8abdb0d9d41422bd
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-09-23 07:39:32 +0800

    Наработки по фильтру.

---
+++ BulletinBoard/BulletinBoard/ViewModels/AdvertisementsFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BulletinBoard.ViewModels
+{
+    public class AdvertisementsFilter
+    {
+        public uint MinPrice { get; set; }
+        public uint MaxPrice { get; set; }
+
+        public AdvertisementsFilter(uint minPrice = 0, uint maxPrice = Int32.MaxValue)
+        {
+            MinPrice = minPrice;
+            MaxPrice = maxPrice;
+        }
+    }
+}
---
+++ BulletinBoard/BulletinBoard/ViewModels/AdvertisementsListPage.cs
@@ -0,0 +1,19 @@
+﻿using BulletinBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BulletinBoard.ViewModels
+{
+    public class AdvertisementsListPage
+    {
+        public IEnumerable<Advertisement> Advertisements { get; set; }
+        public AdvertisementsFilter Filter { get; set; }
+        public AdvertisementsListPage(IEnumerable<Advertisement> advertisements, AdvertisementsFilter filter)
+        {
+            Advertisements = advertisements;
+            Filter = filter;
+        }
+    }
+}
