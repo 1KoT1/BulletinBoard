576d2393c7072f7a5af4dee12c99070fb217281d
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-09-23 21:25:25 +0800

    Создан проект для модульных тестов.

---
+++ BulletinBoard/BulletinBoard.UnitTests/AdvertisementsControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletinBoard.UnitTests
+{
+    [TestFixture]
+    class AdvertisementsControllerTest
+    {
+        [Test]
+        public void List_GetView_ItsOkViewModelIsAdvertisementsListPagew()
+        {
+            var a = "Проба";
+        }
+    }
+}
