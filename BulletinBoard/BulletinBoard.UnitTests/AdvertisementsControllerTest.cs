d81acff12b674012ad90686d00c72c72c8485928
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-09-24 15:58:42 +0800

    Проверяю, что действие List всегда возвращает список объявлений и по умолчанию объявления отсортированы по дате публикации.

--- BulletinBoard/BulletinBoard.UnitTests/AdvertisementsControllerTest.cs
+++ BulletinBoard/BulletinBoard.UnitTests/AdvertisementsControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using BulletinBoard.Controllers;
+using BulletinBoard.Models;
 using BulletinBoard.ViewModels;
 using NUnit.Framework;
 using System;
@@ -25,6 +26,42 @@ namespace BulletinBoard.UnitTests
 
             Assert.IsInstanceOf<ViewResult>(result);
             Assert.IsInstanceOf<AdvertisementsListPage>((result as ViewResult).Model);
+        }
+
+        [Test]
+        public void List_GetView_ItsOkViewContanesListOfAdvertisements(
+            [Values(Sort.Name, Sort.Price, Sort.PublishDate)]Sort sort,
+            [Values((uint)0, uint.MaxValue, (uint)3)]uint minPrice,
+            [Values((uint)0, uint.MaxValue, (uint)6437)]uint maxPrice)
+        {
+            var controller = new AdvertisementsController();
+
+            var result = controller.List();
+
+            var advertisements = ((result as ViewResult).Model as AdvertisementsListPage).Advertisements;
+            if (!advertisements.Any())
+            {
+                Assert.Fail("Действаие List должно вернуть список объявлений.");
+            }
+        }
+
+        [Test]
+        public void List_GetViewDefault_ItsOkViewContanesListOfAdvertisementsOrderedByPublishDate()
+        {
+            var controller = new AdvertisementsController();
+
+            var result = controller.List();
+
+            var advertisements = ((result as ViewResult).Model as AdvertisementsListPage).Advertisements;
+            Advertisement backAdvertisment = null;
+            foreach (var adv in advertisements)
+            {
+                if(backAdvertisment != null && adv.PublishDate < backAdvertisment.PublishDate)
+                {
+                    Assert.Fail();
+                }
+                backAdvertisment = adv;
+            }
         }
     }
 }
